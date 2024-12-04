007057a2cce3b8c33b7d9924b575dd33c3446f79
JuliaG03 <julia.grasu@s.unibuc.ro>
2024-12-04 14:12:06 +0200

    added functions for project

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -5,8 +5,10 @@ import './Project.sol';
 
 contract Crowdfunding{
 
-
-// Anyone can start a funding project 
+// ---to do 
+// Anyone can start a funding project    (done)
+// get all project list  (done)
+//filter project 
 event projectStarted(
     address projectContractAddress ,
     address creator,
@@ -18,7 +20,37 @@ event projectStarted(
     string projectTitle,
     string projectDesc
 );
+
+
  Project[] private projects;
- function createProject() public {
+
+
+function createProject(
+    uint256 minimumContribution,
+    uint256 deadline,
+    uint256 targetContribution,
+    string memory projectTitle,
+    string memory projectDesc
+ ) public {
+   deadline = block.timestamp+deadline;
+   Project newProject = new Project(msg.sender,minimumContribution,deadline,targetContribution,projectTitle,projectDesc);
+   projects.push(newProject);
+ 
+ emit projectStarted(
+    address(newProject) ,
+    msg.sender,
+    minimumContribution,
+    deadline,
+    targetContribution,
+    0,
+    0,
+    projectTitle,
+    projectDesc
+ );
+
  }
+
+function returnAllProjects() external view returns(Project[] memory){
+   return projects;
+}
 }
--- contracts/Project.sol
+++ contracts/Project.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
-
+// --- to do
 // Anyone can contribute
 // If targeted contribution is not reached - we end the project
 // If raised amount is not equal to the targeted contribution
@@ -21,7 +21,7 @@ contract Project{
         Successful
     }
 
-
+// variables: 
     address payable public creator;
     uint256 minimumContribution;
     uint256 deadline;
@@ -32,4 +32,40 @@ contract Project{
     string projectDes;
     State public state = State.Fundraising; 
     mapping (address => uint) public contributiors;
+
+// modifiers: 
+    
+    // Modifiers
+    modifier isCreator(){
+        require(msg.sender == creator,'You dont have access to perform this operation !');
+        _;
+    }
+    modifier validateExpiry(State _state){
+        require(state == _state,'Invalid state');
+        require(block.timestamp < deadline,'Deadline has passed !');
+        _;
+    }
+   constructor(
+       address _creator,
+       uint256 _minimumContribution,
+       uint256 _deadline,
+       uint256 _targetContribution,
+       string memory _projectTitle,
+       string memory _projectDes
+   ) {
+       creator = payable(_creator);
+       minimumContribution = _minimumContribution;
+       deadline = _deadline;
+       targetContribution = _targetContribution;
+       projectTitle = _projectTitle;
+       projectDes = _projectDes;
+       raisedAmount = 0;
+   }
+ function contribute() public validateExpiry(State.Fundraising) payable {
+     require(msg.value >= minimumContribution,'Contribution amount is too low !');
+
+          if(contributiors[msg.sender] == 0){
+         
+     }
+ }
 }
