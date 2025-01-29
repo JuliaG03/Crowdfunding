b613edbacbdf7f448842dcc59442172b0f640af2
flaviaf7 <fota.flavia@yahoo.com>
2025-01-29 16:34:18 +0200

    completed contribute & refund functions

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
-// --- to do
 // Anyone can contribute
 // If targeted contribution is not reached - we end the project
 // If raised amount is not equal to the targeted contribution
@@ -21,51 +20,100 @@ contract Project{
         Successful
     }
 
-// variables: 
-    address payable public creator;
-    uint256 minimumContribution;
-    uint256 deadline;
-    uint256 targetContribution; // the contribution is required to be at least this amount
-    uint public completeAt;
-    uint256 raisedAmount; // Total raised untill present moment
-    string projectTitle;
-    string projectDes;
-    State public state = State.Fundraising; 
-    mapping (address => uint) public contributiors;
+    // structs
 
-// modifiers: 
-    
-    // Modifiers
-    modifier isCreator(){
-        require(msg.sender == creator,'You dont have access to perform this operation !');
-        _;
+    struct WithdrawRequest{
+        string description;
+        uint256 amount;
+        uint256 noOfVotes;
+        mapping(address => uint256) voters;
+        bool isCompleted;
+        address payable recipient;
     }
-    modifier validateExpiry(State _state){
-        require(state == _state,'Invalid state');
-        require(block.timestamp < deadline,'Deadline has passed !');
-        _;
-    }
-   constructor(
-       address _creator,
-       uint256 _minimumContribution,
-       uint256 _deadline,
-       uint256 _targetContribution,
-       string memory _projectTitle,
-       string memory _projectDes
-   ) {
-       creator = payable(_creator);
-       minimumContribution = _minimumContribution;
-       deadline = _deadline;
-       targetContribution = _targetContribution;
-       projectTitle = _projectTitle;
-       projectDes = _projectDes;
-       raisedAmount = 0;
-   }
- function contribute() public validateExpiry(State.Fundraising) payable {
-     require(msg.value >= minimumContribution,'Contribution amount is too low !');
 
-          if(contributiors[msg.sender] == 0){
-         
-     }
- }
+    // variables: 
+        address payable public creator;
+        uint256 public minimumContribution;
+        uint256 public deadline;
+        uint256 public targetContribution; // the contribution is required to be at least this amount
+        uint public completeAt;
+        uint256 public raisedAmount; // Total raised untill present moment
+        uint256 public noOfContributers;
+        string public projectTitle;
+        string public projectDes;
+        State public state = State.Fundraising; 
+        mapping (address => uint) public contributors;
+
+    // modifiers: 
+        
+        // Modifiers
+        modifier isCreator(){
+            require(msg.sender == creator,'You dont have access to perform this operation !');
+            _;
+        }
+        modifier validateExpiry(State _state){
+            require(state == _state,'Invalid state');
+            require(block.timestamp < deadline,'Deadline has passed !');
+            _;
+        }
+
+        // events:
+
+        //event that will be emitted when funding is received
+        event FundingReceived(address contributor, uint amount, uint currentTotal);
+        //event that will be emitted when contributor votes for withdraw request
+        event WithdrawVote(address contributor, uint amount, uint currentTotal);
+
+
+    constructor(
+        address _creator,
+        uint256 _minimumContribution,
+        uint256 _deadline,
+        uint256 _targetContribution,
+        string memory _projectTitle,
+        string memory _projectDes
+    ) {
+        creator = payable(_creator);
+        minimumContribution = _minimumContribution;
+        deadline = _deadline;
+        targetContribution = _targetContribution;
+        projectTitle = _projectTitle;
+        projectDes = _projectDes;
+        raisedAmount = 0;
+    }
+
+
+    function contribute() public validateExpiry(State.Fundraising) payable {
+        require(msg.value >= minimumContribution, 'Contribution amount is too low !');
+
+        if(contributors[msg.sender] == 0){
+            noOfContributers++;
+        }
+        contributors[msg.sender] += msg.value;
+        raisedAmount += msg.value;
+        emit FundingReceived(msg.sender, msg.value, raisedAmount);
+        checkFundingCompleteOrExpire();
+    }
+
+    function checkFundingCompleteOrExpire() internal {
+        if(raisedAmount >= targetContribution){
+            state = State.Successful;
+        }else if(block.timestamp > deadline){
+            state = State.Expired;
+        }
+        completeAt = block.timestamp;
+    }
+
+
+    function getContractBalance() public view returns(uint256){
+        return address(this).balance;
+    }
+
+    function requestRefund() public validateExpiry(State.Expired) returns(bool) {
+        require(contributors[msg.sender] > 0, 'You dont have any contributed amount!');
+        address payable user = payable(msg.sender);
+        user.transfer(contributors[msg.sender]);
+        contributors[msg.sender] = 0;
+        return true;
+    }
 }
